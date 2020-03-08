db89ca549e09a0e5366bcdd3481a8383f4d58920
EnerCom <enercom@Temporarys-MacBook-Pro.local>
2020-03-08 15:37:54 -0600

    concat changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require ('gulp'),
-	gutil = require ('gulp-util');
+	gutil = require ('gulp-util'), 
+	concat = require('gulp-concat');
+
+var jsSources = [
+'development/customtheme-master/js/bootstrap.min.js','development/customtheme-master/js/main.min.js'
+];
 
 // gulp.task('log', function () {
 // 	gutil.log('workflow demonstration');
@@ -8,3 +13,7 @@ var gulp = require ('gulp'),
 // gulp.task('coffee', function () {
 // 	gulp.src('components/coffee/tagline.coffee').pipe (coffee({ bare: true})).on('error',gutil.log)
 // });
+
+gulp.task('js', function () {
+	gulp.src(jsSources).pipe(concat('script.js')).pipe(gulp.dest('development/customtheme-master/js'))
+});
