daab9e96072d83eebb81d78775952d691ff33e34
EnerCom <enercom@Temporarys-MacBook-Pro.local>
2020-03-08 15:07:31 -0600

    added files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require ('gulp'),
 	gutil = require ('gulp-util');
 
-gulp.task('log', function () {
-	gutil.log('workflow demonstration');
-});
+// gulp.task('log', function () {
+// 	gutil.log('workflow demonstration');
+// });
+
+// gulp.task('coffee', function () {
+// 	gulp.src('components/coffee/tagline.coffee').pipe (coffee({ bare: true})).on('error',gutil.log)
+// });
